ca3ae72507a7bbbc6b0975f8a03d4f78027ae4ea
Ricardo Rodrigues <148401739+RicardoRodrigues89@users.noreply.github.com>
2023-11-22 09:20:47 -0300

    Atualizando funções e colocando detalhes ao css.

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+const enviaQr = document.querySelector('.botaoEnviar');
+
 function gerarQRCode() {
-    var enderecoInput = document.getElementById('endereco');
-    var qrImg = document.getElementById('qrCodeImg');
-    var enderecoValor = enderecoInput.value;
-  
-    if (enderecoValor.trim() !== '') {
-      qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + encodeURIComponent(enderecoValor);
-    } else {
-      alert('Por favor, insira um endereço válido.');
-    }
+  var enderecoInput = document.getElementById('endereco');
+  var qrImg = document.getElementById('qrCodeImg');
+  var enderecoValor = enderecoInput.value;
+
+  if (enderecoValor.trim() !== '') {
+    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + encodeURIComponent(enderecoValor);
+  } else {
+    alert('Por favor, insira um endereço válido.');
   }
+}
+
+enviaQr.addEventListener("click",function(){
+  gerarQRCode();
+
+})
