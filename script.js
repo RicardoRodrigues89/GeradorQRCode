6ff0c66e07889c16675cc19b1adeff3800566932
Ricardo Rodrigues <148401739+RicardoRodrigues89@users.noreply.github.com>
2023-11-21 21:36:00 -0300

    Gerador de QR Code feito

---
+++ script.js
@@ -0,0 +1,11 @@
+function gerarQRCode() {
+    var enderecoInput = document.getElementById('endereco');
+    var qrImg = document.getElementById('qrCodeImg');
+    var enderecoValor = enderecoInput.value;
+  
+    if (enderecoValor.trim() !== '') {
+      qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + encodeURIComponent(enderecoValor);
+    } else {
+      alert('Por favor, insira um endereço válido.');
+    }
+  }
